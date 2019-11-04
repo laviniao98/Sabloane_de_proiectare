d8fee12a3e7f6dc6868ef3fcdeacb14465dd4f6e
laviniao98 <51117712+laviniao98@users.noreply.github.com>
2019-11-04 13:50:57 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,34 +5,13 @@ public class Main {
 
 
 	public String Author;
-
-
-
 	public String Book;
-
-
-
 	public String Chapter;
-
-
-
 	public String SubChapter;
-
-
-
 	public String Paragraph;
-
-
-
 	public String Image;
 
-
-
 	public String Table;
-
-	
-
-
 
 	public Main(String A, String B, String C, String SC, String P, String I, String T)
 
@@ -70,7 +49,7 @@ public class Main {
 
 
 
-	public static void main(String[] args) {
+
 
 		// TODO Auto-generated method stub
 /*
@@ -100,7 +79,7 @@ public class Main {
 
 	int indexOfImage2= chpOneOne.createimageList("im2");
 */
-
+/*
 	    Book noapteBuna = new Book("Noapte buna copii.!");
 	    Author Gh = new Author("Radu Gheo");
 	    noapteBuna.addAuthor(Gh);
@@ -112,9 +91,62 @@ public class Main {
 	    noapteBuna.addContent(new Paragraph("Multumesc"));
 	    
 	    noapteBuna.print();
+*/
+/*
+		public static void main(String[] args) throws Exception {
+			long startTime = System.currentTimeMillis();
+			ImageProxy img1 = new ImageProxy("Pamela Anderson");
+			ImageProxy img2 = new ImageProxy("Kim Kardashian");
+			ImageProxy img3 = new ImageProxy("Kirby Griffin");
+			Section playboyS1 = new Section("Front Cover");
+			playboyS1.add(img1);
+			Section playboyS2 = new Section("Summer Girls");
+			playboyS2.add(img2);
+			playboyS2.add(img3);
+			Book playboy = new Book("Playboy");
+			playboy.addContent(playboyS1);
+			playboy.addContent(playboyS2);
+			long endTime = System.currentTimeMillis();
+			System.out.println("Creation of the content took " + (endTime -
+			startTime) + " milliseconds");
+			startTime = System.currentTimeMillis();
+			playboyS1.print();
+			endTime = System.currentTimeMillis();
+			System.out.println("Printing of the section 1 took " + (endTime -
+			startTime) + " milliseconds");
+			startTime = System.currentTimeMillis();
+			playboyS1.print();
+			endTime = System.currentTimeMillis();
+			System.out.println("Printing again the section 1 took " + (endTime -
+			startTime) + " milliseconds");
+			}
 
 	//book.print();
+	*/
+	public static void main(String[] args) throws Exception {
+		Section cap1 = new Section("Capitolul 1");
+		Paragraph p1 = new Paragraph("Paragraph 1");
+		cap1.add(p1);
+		Paragraph p2 = new Paragraph("Paragraph 2");
+		cap1.add(p2);
+		Paragraph p3 = new Paragraph("Paragraph 3");
+		cap1.add(p3);
+		Paragraph p4 = new Paragraph("Paragraph 4");
+		cap1.add(p4);
+		System.out.println("Printing without Alignment");
+
+		System.out.println();
+		cap1.print();
+		p1.setAlignStrategy(new CenterStrategy());
+		p2.setAlignStrategy(new RightStrategy());
+		p3.setAlignStrategy(new LeftStrategy());
+		System.out.println();
+		System.out.println("Printing with Alignment");
+		System.out.println();
+		cap1.print();
+		}
+	 
 	}
 
-}
+
 
